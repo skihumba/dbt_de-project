41d42a8d8e6ebcb12f832edac493bd95fabf065d
sam <abmuhiks@yahoo.com>
2024-03-28 17:24:14 +0000

    generated docs

--- analyses/utils.sql
+++
@@ -1,6 +0,0 @@
--- Generate model.yml 
-{% set models_to_generate = codegen.get_models(directory='staging', prefix='stg_') %}
-{{ codegen.generate_model_yaml(
-    model_names = models_to_generate
-) }}
-
