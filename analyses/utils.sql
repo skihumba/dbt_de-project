96f14666e8dabda9483c1a3aa92658fad19996f9
sam <abmuhiks@yahoo.com>
2024-03-28 17:17:36 +0000

    updated schema.yml

---
+++ analyses/utils.sql
@@ -0,0 +1,6 @@
+-- Generate model.yml 
+{% set models_to_generate = codegen.get_models(directory='staging', prefix='stg_') %}
+{{ codegen.generate_model_yaml(
+    model_names = models_to_generate
+) }}
+
